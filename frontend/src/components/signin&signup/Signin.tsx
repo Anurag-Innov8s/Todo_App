c5ce0d72cc1caa347441ca6c9b501ce8ce2dd8c8
Anurag Kumar <anuraganu202016@gmail.com>
2024-03-16 02:01:19 +0530

    added login & signup functionality

--- frontend/src/components/signin&signup/Signin.tsx
+++ frontend/src/components/signin&signup/Signin.tsx
@@ -1,20 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'; 
 import './Signin.css';
-
+import { EmailRounded, LockRounded } from '@mui/icons-material';
+import axios from 'axios';
 const Signin: React.FC = () => {
+  const navigate = useNavigate();
+  const [email,setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async (e:any)=>{
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/signin',{
+        email,
+        password
+      });
+      
+      console.log(response.data);
+      setEmail("");
+      setPassword("");
+      navigate("/signup");
+    } catch (error:any) {
+      alert("Error while login")
+    }
+  }
   return (
     <>
         <div className="form-wrapper sign-in">
-          <form action="">
+          <form action="" onSubmit={submitHandler}>
             <h2>ToDo App</h2>
             <h2>Login</h2>
             <div className="input-group">
-              <input type="text" required />
-              <label htmlFor="">Username</label>
+            <div style={{marginRight:"6px",marginTop: '10px'}}>
+              <EmailRounded/>
+              </div>
+              <input type="text" required value={email} onChange={e=>setEmail(e.target.value)}/>
+              <label htmlFor="">Email</label>
             </div>
             <div className="input-group">
-              <input type="password" required />
+            <div style={{marginRight:"6px",marginTop: '10px'}}>
+              <LockRounded/>
+              </div>
+              <input type="password" required value={password} onChange={e=>setPassword(e.target.value)}/ >
               <label htmlFor="">Password</label>
             </div>
             <div className="forgot-pass">
--- frontend/src/components/signin&signup/Signup.tsx
+++ frontend/src/components/signin&signup/Signup.tsx
@@ -1,37 +1,70 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import './Signin.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; 
+import './Signup.css'; 
+import { Person2Rounded, EmailRounded, LockRounded } from '@mui/icons-material';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';const Signup: React.FC = () => {
 
-const Signin: React.FC = () => {
+  const navigate = useNavigate();
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const submitHandler = async (e:any)=>{
+  e.preventDefault();
+  try {
+    const response = await axios.post('http://localhost:5000/signup',{
+      name,
+      email,
+      password
+    });
+    
+    console.log(response.data);
+    setName("");
+    setEmail("");
+    setPassword("");
+    navigate("/");
+  } catch (error:any) {
+    alert("Error while signup")
+  }
+}
   return (
     <>
       <div className="wrapper">
         <div className="form-wrapper sign-up">
-          <form action="">
+          <form action="" onSubmit={submitHandler}>
             <h2>ToDo App</h2>
             <h2>Sign Up</h2>
             <div className="input-group">
-              <input type="text" required />
+            <div style={{marginRight:"6px",marginTop: '10px'}}>
+              <Person2Rounded/>
+              </div>
+              <input type="text" required value={name} onChange={e=>setName(e.target.value)}/>
               <label htmlFor="">Username</label>
             </div>
             <div className="input-group">
-              <input type="email" required />
+            <div style={{marginRight:"6px",marginTop: '10px'}}>
+              <EmailRounded/>
+              </div>
+              <input type="email" required value={email} onChange={e=>setEmail(e.target.value)}/>
               <label htmlFor="">Email</label>
             </div>
             <div className="input-group">
-              <input type="password" required />
+            <div style={{marginRight:"6px",marginTop: '10px'}}>
+              <LockRounded/>
+              </div>
+              <input type="password" required value={password} onChange={e=>setPassword(e.target.value)}/>
               <label htmlFor="">Password</label>
             </div>
             <button type="submit" className="btn">Sign Up</button>
             {/* Link to sign-in form */}
             <div className="sign-link">
-              <p>Already have an account? <Link to="/signin" className="signIn-link">Sign In</Link></p>
+              <p>Already have an account? <Link to="/" className="signIn-link">Sign In</Link></p>
             </div>
           </form>
         </div>
-      </div>
+      </div> 
     </>
   );
 };
 
-export default Signin;
+export default Signup;
